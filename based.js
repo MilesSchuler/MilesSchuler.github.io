3e5fa12bf1b9cfbfdbb8ec623d4cb119b043506a
slickOnGit <milesdz@icloud.com>
2022-12-14 16:29:16 -0800

    thingy

--- based.js
+++ based.js
@@ -1,3 +1,9 @@
-$.ajax({
-    url: "https://en.wikipedia.org/wiki/Special:Random"
-}).done(results => {console.log(results)});
+function refresh() {
+    $.ajax({url: 'https://en.wikipedia.org/api/rest_v1/page/random/summary'}).done(res => {
+        //let obj = JSON.parse(res);
+        console.log(res);
+        //console.log(obj)
+        $('#data').html(res.title)
+        $("#info").html(res.extract.split(". ")[0])
+    });
+}
