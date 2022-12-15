1ef76c95ee7f51ee9e0792cead329dcb88721fc2
slickOnGit <milesdz@icloud.com>
2022-12-15 13:19:27 -0800

    based update

--- based.js
+++ based.js
@@ -1,11 +1,60 @@
-refresh();
+var db; 
+var app;
+
+window.onload = () => {
+    initDatabase();
+    refresh();
+}
+
+$('.choice').on('click', e => {
+    refresh();
+    insert($('#data').html(), parseInt(e.target.id));
+})
+window.addEventListener("keypress", e => {
+    refresh();
+    if (e.key == "1" || e.key == "2" || e.key == "3") {
+        insert($('#data').html(), (parseInt(e.key) - 2) * -1);
+    }
+});
+
 function refresh() {
     $.ajax({url: 'https://en.wikipedia.org/api/rest_v1/page/random/summary'}).done(res => {
-        //let obj = JSON.parse(res);
-        console.log(res);
-        //console.log(obj)
         $('#data').html(res.title)
         $("#info").html(res.extract.split(". ")[0])
     });
 }
+function initDatabase() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyB7wEvnInVLsxdfPuPEfBQfVteR4Clce7w",
+        projectId: "based-or-cringe-f022a",
+        appID: "based-or-cringe-f022a"
+    };
 
+    app = firebase.initializeApp(firebaseConfig);
+
+    db = firebase.firestore();
+}
+/*function readDatabase() {
+    db.collection("items").get().then((res) => {
+        res.forEach(item => {
+            console.log(item.data());
+        });
+    });
+}*/
+function insert(key, value) {
+    dbLength().then(len => {
+        db.collection("items").doc(len.toString()).set({
+            key: key.toLowerCase(),
+            value: value
+        });
+    });
+}
+async function dbLength() {
+    return await db.collection("items").get().then((res) => {
+        let i = 0;
+        res.forEach(item => {
+            i++;
+        });
+        return i+1;
+    });
+}
