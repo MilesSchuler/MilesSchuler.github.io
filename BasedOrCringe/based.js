30457f1343f3dd6e1ed02bd02a6c8f1431aca568
slickOnGit <milesdz@icloud.com>
2023-01-05 07:21:05 -0800

    Update based.js

--- BasedOrCringe/based.js
+++ BasedOrCringe/based.js
@@ -71,12 +71,12 @@ function insert(key, value) {
 
 String.prototype.hashCode = function() {
     var hash = 0,
-      i, chr;
+    i, chr;
     if (this.length === 0) return hash;
     for (i = 0; i < this.length; i++) {
-      chr = this.charCodeAt(i);
-      hash = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
     }
     return hash;
-  }
+}
