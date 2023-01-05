257db2925cf1fa420377f20ea8543425a0093bfa
slickOnGit <milesdz@icloud.com>
2023-01-05 07:20:17 -0800

    Update based.js

--- BasedOrCringe/based.js
+++ BasedOrCringe/based.js
@@ -63,19 +63,20 @@ function initDatabase() {
     });
 }*/
 function insert(key, value) {
-    dbLength().then(len => {
-        db.collection("items").doc(len.toString()).set({
-            key: key.toLowerCase(),
-            value: value
-        });
+    db.collection("items").doc(key.hashCode()).set({
+        key: key.toLowerCase(),
+        value: value
     });
 }
-async function dbLength() {
-    return await db.collection("items").get().then((res) => {
-        let i = 0;
-        res.forEach(item => {
-            i++;
-        });
-        return i+1;
-    });
-}
+
+String.prototype.hashCode = function() {
+    var hash = 0,
+      i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
