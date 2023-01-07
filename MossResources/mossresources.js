d8a5b6b21fea6ca4858d08948c6ff2315e8e517c
slickOnGit <milesdz@icloud.com>
2023-01-06 17:08:39 -0800

    working!

--- MossResources/mossresources.js
+++ MossResources/mossresources.js
@@ -1,3 +1,5 @@
+BONEMEAL_PER_MOSS_WITH_TALL_GRASS = 0.1146875
+BONEMEAL_PER_MOSS_WITHOUT_TALL_GRASS = 0.1133482143
 corner_buttons = ["mb-00", "mb-06", "mb-60", "mb-66"]
 
 window.onload = () => {
@@ -30,7 +32,20 @@ window.onload = () => {
     }
 }
 
-function allMossOn() {
+document.onclick = function(event) {
+    total = 0
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7; j++) {
+            b = document.getElementById("mb-" + i + j)
+            if (b.name == "on") {
+                total++
+            }
+        }
+    }
+    document.getElementById("bonemeal_calc").textContent = "Bonemeal Per Cycle: " + total * bonemeal_per_moss()
+};
+
+function all_moss_on() {
     for (i = 0; i < 7; i++) {
         for (j = 0; j < 7; j++) {
             id = "mb-" + i + j
@@ -43,14 +58,45 @@ function allMossOn() {
     }
 }
 
-function resetMoss() {
-    for (i = 0; i < 7; i++) {
-        for (j = 0; j < 7; j++) {
-            id = "mb-" + i + j
-            button = document.getElementById(id)
-            button.name = "off"
-            $("#" + id).css("backgroundColor", "#9d9d9d")
+function three_by_three_on() {
+    reset_moss()
+    for (i = 2; i < 5; i++) {
+        for (j = 2; j < 5; j++) {
+            id = "mb-" + i + j;
+            button = document.getElementById(id);
+            button.name = "on";
+            $("#" + id).css("backgroundColor", "#00ce15");
         }
     }
 }
 
+function five_by_five_on() {
+    reset_moss()
+    for (i = 1; i < 6; i++) {
+        for (j = 1; j < 6; j++) {
+            id = "mb-" + i + j;
+            button = document.getElementById(id);
+            button.name = "on";
+            $("#" + id).css("backgroundColor", "#00ce15");
+        }
+    }
+}
+
+function reset_moss() {
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7; j++) {
+            id = "mb-" + i + j;
+            button = document.getElementById(id);
+            button.name = "off";
+            $("#" + id).css("backgroundColor", "#9d9d9d");
+        }
+    }
+}
+
+function bonemeal_per_moss() {
+    if (document.getElementById("use-tall-grass").checked) {
+        return BONEMEAL_PER_MOSS_WITH_TALL_GRASS;
+    } else {
+        return BONEMEAL_PER_MOSS_WITHOUT_TALL_GRASS;
+    }
+}
